54416051b8da54bf20fb7ce68a1ea248e850bf5b
sokcuri <sokcuri@gmail.com>
2021-03-19 12:28:50 +0900

    build to one bundle

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   mount: {
     public: '/',
@@ -9,17 +11,40 @@ module.exports = {
     '@snowpack/plugin-typescript',
     '@snowpack/plugin-sass',
     '@snowpack/plugin-babel',
-    '@snowpack/plugin-optimize'
+    ['@snowpack/plugin-webpack', {
+      outputPattern: {
+        js: "index.js",
+        css: "index.css",
+      },
+      extendConfig: config => {
+        delete config.optimization.splitChunks;
+        delete config.optimization.runtimeChunk;
+        config.module.rules[0] = {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: { presets: ['@babel/preset-env'] }
+            },
+            {
+              loader: path.resolve(__dirname, './node_modules/@snowpack/plugin-webpack/plugins/import-meta-fix.js')
+            }
+          ]
+        }
+        return config;
+      }
+    }]
   ],
   alias: {
     '~': './src',
     '@': './src'
   },
-  install: [],
-  installOptions: {},
+  knownEntrypoints: [],
+  packageOptions: {},
   devOptions: {
+    output: 'stream'
     // open: 'none'
   },
   buildOptions: {},
-  proxy: {}
 };
